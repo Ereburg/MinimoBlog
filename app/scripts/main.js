b05688b6f1327f991241d1c4ad946d3db08c4d3d
Eugene <eugene.kotsarev@gmail.com>
2019-12-22 04:42:19 +0300

    new update 1.2 move to scss syntax; add some scripts and include some stylings;

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -1,4 +1,55 @@
 document.addEventListener("DOMContentLoaded", () => {
-	// Custom JS
-	console.log('Hello, World!');
+
+	// Добавляем класс scroll в шапку
+	const header = document.querySelector('.header');
+	const buttonPosts = document.querySelector('.posts__btn');
+	const articlesPosts = document.querySelectorAll('.posts__article');
+	const gridsPosts = document.querySelectorAll('.posts__grid');
+	const postItemCopyOne = articlesPosts[0].cloneNode(true);
+	const postItemCopyTwo = articlesPosts[1].cloneNode(true);
+	const postItemCopyThree = articlesPosts[2].cloneNode(true);
+	const postItemCopyFour = articlesPosts[3].cloneNode(true);
+
+	window.addEventListener('scroll', () => {
+		let y = window.pageYOffset;
+
+		if (y > 55) {
+			header.classList.add('scroll');
+		} else {
+			header.classList.remove('scroll');
+		}
+	});
+
+	buttonPosts.addEventListener('click', () => {
+		gridsPosts[1].append(postItemCopyThree);
+		gridsPosts[1].append(postItemCopyFour);
+
+		gridsPosts[1].append(postItemCopyOne);
+		gridsPosts[1].append(postItemCopyTwo);
+		buttonPosts.remove();
+	});
+
+
+	// Меню
+	var navButton = document.querySelector('.nav__button');
+	var navMenu = document.querySelector('.nav__menu');
+	const burgerItrem = document.querySelectorAll('.burger__item');
+
+
+	navButton.addEventListener('click', function (evt) {
+		evt.preventDefault();
+		if (navMenu.classList.contains('nav__menu--show')) {
+			navMenu.classList.remove('nav__menu--show');
+			burgerItrem[0].classList.remove('burger__item--first');
+			burgerItrem[1].classList.remove('burger__item--second');
+			burgerItrem[2].classList.remove('burger__item--last');
+			navButton.classList.remove('nav__button--close');
+		} else {
+			navMenu.classList.add('nav__menu--show');
+			burgerItrem[0].classList.add('burger__item--first');
+			burgerItrem[1].classList.add('burger__item--second');
+			burgerItrem[2].classList.add('burger__item--last');
+			navButton.classList.add('nav__button--close');
+		}
+	});
 });
